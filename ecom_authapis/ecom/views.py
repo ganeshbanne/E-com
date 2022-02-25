f1df28e3786793327a38bc2cb15733e611279453
ganeshbanne <theganeshbanne@gmail.com>
2022-02-25 19:15:45 +0530

    specific permissions added

--- ecom_authapis/ecom/views.py
+++ ecom_authapis/ecom/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from ecom.serializers import ProductSerializer
 from ecom.models import Product
 
 class ProductOperations(ModelViewSet):
+    permission_classes = (AllowAny,)
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        if self.action in ["create","update","partial_update","destroy"]:
+            self.permission_classes =(IsAuthenticated,)
+        return super().get_permissions()
